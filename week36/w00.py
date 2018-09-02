dcdb3c1b5aa79ef8aff6cfbed23f7bb37d43979c
Usman00Khan <Usman000Khan@users.noreply.github.com>
2018-09-02 19:41:50 +0530

    week 36 folder added

---
+++ week36/w00.py
@@ -0,0 +1,9 @@
+#n = input("Enter the natural number")
+#try:
+    #assert n = int(n)
+sum = 0
+for n in [10,100,200]:
+    sum += (n*(n+1))//2
+print ("The sum is",sum)
+'''except:
+    print("Enter a integer")'''
